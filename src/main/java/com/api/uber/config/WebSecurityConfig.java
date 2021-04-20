d3306debd603e8553b2b551586199e1b19ae1385
naresh <agrawal.nar@northeastern.edu>
2021-04-19 20:32:03 -0400

    adding prometheus

--- src/main/java/com/api/uber/config/WebSecurityConfig.java
+++ src/main/java/com/api/uber/config/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable().cors().and()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/login", "/register", "/testHealth").permitAll().
+                .authorizeRequests().antMatchers("/login", "/register", "/health","/backend").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
